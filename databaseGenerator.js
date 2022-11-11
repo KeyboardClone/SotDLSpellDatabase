549ae2e6f6f813e0ed3a3617a42be90b3d34cd5d
BuildTools <unconfigured@null.spigotmc.org>
2022-11-10 22:32:39 -0500

    A javascript program intended to assist in the creation of an sqlite database of Shadow of the Demon Lord spells

---
+++ databaseGenerator.js
@@ -0,0 +1,29 @@
+// initialize or open the sqlite database
+const { QuickDB } = require('quick.db');
+const db = new QuickDB({ filePath: "SotDLSpells.sqlite"});
+
+(async () => {
+    // setting up to reading the input file line by line
+    const lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream('Spells.txt')
+    });
+      
+    // perform specific operations per line within the file
+    lineReader.on('line', function (line) {
+        const entry = line.split(/;/);
+        // console.log(entry);
+
+        // put in an entry into the database, if it already exists
+        // we then update that entry
+        db.set(entry[0], {bookOrigin: entry[1], 
+                            spellTradition: entry[2],
+                            spellType: entry[3],
+                            spellLevel: entry[4],
+                            spellDescription: entry[5]});
+
+    });
+
+    // Testing to see if we can display an entry in the database
+    console.log("PRESENCE");
+    console.log(await db.get("PRESENCE"));
+})();
