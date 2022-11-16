2b93a82894d2bcf22a8093629857d5193095d2d5
BuildTools <unconfigured@null.spigotmc.org>
2022-11-15 22:00:11 -0500

    Completely added the core set of spells to the Spells.txt file

--- databaseGenerator.js
+++ databaseGenerator.js
@@ -24,6 +24,6 @@ const db = new QuickDB({ filePath: "SotDLSpells.sqlite"});
     });
 
     // Testing to see if we can display an entry in the database
-    console.log("PRESENCE");
-    console.log(await db.get("PRESENCE"));
+    console.log("GLOBE OF NEGATION");
+    console.log(await db.get("GLOBE OF NEGATION"));
 })();
